70d60c760c8d3cde97940f90ff4432e9f08d9f58
Richard Chin <richjing@gmail.com>
2016-01-05 23:29:48 +0800

    initial commit. foreground mode example

--- app/src/main/java/com/ilocky/foregroundexample/MainActivity.java
+++ app/src/main/java/com/ilocky/foregroundexample/MainActivity.java
@@ -1,7 +1,17 @@
 package com.ilocky.foregroundexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.ilocky.ILocky;
+import com.ilocky.ILockyActivity;
+import com.ilocky.ILockyPassport;
+import com.ilocky.ILockyService;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ILockyPassport.initialize(this);
+        Log.d("iLocky",ILockyPassport.getDeviceUuid());
+        String passport="{\n" +
+                "  \"passport_id\": 2,\n" +
+                "  \"passport\":\"eyJpbG9ja3lfaWQiOiJjNGI5ZmQ0MyIsInV1aWQiOiJjOGVjYzUwNC1hNmJiLTRjMWYtOTlhMi01ZGE3NDFiYmEwMWUiLCJzdGFydF90aW1lIjoiMjAxNS0xMS0wN1QwMDowMDowMCswODAwIiwic3RvcF90aW1lIjoiMjAyMC0xMS0yMFQwMDowMDowMCswODAwIiwidXNlX2xpbWl0IjowLCJyZmlkX2R1cmF0aW9uIjoxMDAwLCJhY3Rpb24iOiJvcGVuIGRvb3IiLCJyZXZva2VfbGVnYWN5Ijp0cnVlfQ==\"\n" +
+                "}";
+        try {
+            ILockyPassport.importPassport(passport);
+        } catch (ILockyPassport.ImportPassportException e) {
+            e.printStackTrace();
+        }
+//        ILocky.setLocale(Locale.SIMPLIFIED_CHINESE);
+//        ILocky.setLocale(Locale.TRADITIONAL_CHINESE);
+//        ILocky.setLocale(Locale.ENGLISH);
+        findViewById(R.id.ivKey).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(MainActivity.this, ILockyActivity.class), 0);
+            }
+        });
+
+
     }
 }
